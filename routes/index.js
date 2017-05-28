7ce96d4bcbd233f612308da12e860b8f2ec9661e
Ed O'Connell <eoconnell44@gmail.com>
2017-05-28 15:29:22 -0400

    added data

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var db = require('../db/queries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Eds run' });
+  res.render('index', { title: 'Express' });
 });
 
 
